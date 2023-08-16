016970829c967443418dfab2f9d76cba2abf7bc6
Ian <pixdo.et@gmail.com>
2023-08-16 22:44:45 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+"""
+    receive.py - server code for receiving & parsing
+
+"""
+
+#!/usr/bin/env python3
+
+import cv2
+import sys
+
+
+faceCascade = cv2.CascadeClassifier(
+    f"{cv2.data.haarcascades}haarcascade_frontalface_default.xml"
+)
+
+video_capture = cv2.VideoCapture(0)
+
+video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
+video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
+h = video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+w = video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+while True:
+    # Capture frame-by-frame
+    ret, frames = video_capture.read()
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE,
+    )
+    # Draw a rectangle around the faces
+    for x, y, w, h in faces:
+        cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(
+            frames,
+            "Person",
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+        )
+        print(f"x: {x}; y: {y}")
+        cv2.putText(
+            frames,
+            f"Count: {len(faces)}",
+            (1050,680),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+        )
+    # Display the resulting frame
+    cv2.imshow(
+        f"Lapsap | Press Q to quit",
+        frames,
+    )
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
