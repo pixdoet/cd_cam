3d1403022442b5b7a69ae2f4905a9cd9beca9b27
Ian Hiew <pixdoet@Ians-MacBook-Pro.local>
2023-12-04 17:41:13 +0800

    Add CLI arguments

--- main.py
+++ main.py
@@ -6,19 +6,52 @@
 #!/usr/bin/env python3
 
 import cv2
-import sys
+import getopt, sys
+
+# need to set default height b4 args...
+h = 640
+w = 80
+usefix = False
+camera = 1
+
+# cli arguments
+argumentList = sys.argv[1:]
+options = "hwfc:"
+long_options = ["height", "width", "usefix", "camera"]
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-h", "--height"):
+            h = currentValue
+        elif currentArgument in ("-w", "--width"):
+            w = currentValue
+        elif currentArgument in ("-f", "--usefix"):
+            if currentValue:
+                usefix = True
+        elif currentArgument in ("-c", "--camera"):
+            camera = currentValue
+
+except getopt.error as e:
+    print(e)
 
 
+# import face cascade
 faceCascade = cv2.CascadeClassifier(
     f"{cv2.data.haarcascades}haarcascade_frontalface_default.xml"
 )
 
-video_capture = cv2.VideoCapture(1)
+video_capture = cv2.VideoCapture(camera)
 
-#video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
-#video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
-h = 640#video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-w =80#video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+# can't remember what this is for, probably for older opencv versions?
+if usefix:
+    video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
+    video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)
+    video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    video_capture.get(cv2.CAP_PROP_FRAME_WIDTH)
 
 while True:
     # Capture frame-by-frame
@@ -35,34 +68,30 @@ while True:
     for x, y, w, h in faces:
         cv2.rectangle(frames, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(
-            frames,
-            "Person",
-            (x, y - 10),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 255, 0),
-            2
+            frames, "Person", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2
         )
         print(f"x: {x}; y: {y}")
         cv2.putText(
             frames,
             f"Count: {len(faces)}",
-            (50,680),
+            (50, 680),
             cv2.FONT_HERSHEY_SIMPLEX,
             1,
             (0, 255, 0),
-            3
+            3,
         )
+
+        # crowd detected!
         if len(faces) > 3:
             print("Crowd detected!")
             cv2.putText(
                 frames,
                 "Crowd Detected!",
-                (50,580),
+                (50, 580),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 1,
                 (0, 255, 0),
-                3
+                3,
             )
     # Display the resulting frame
     cv2.imshow(
